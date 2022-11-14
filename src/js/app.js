44e02cfe3fefbeb6fafe28980456475fa305a18c
Malins062 <6328673@gmail.com>
2022-11-14 15:09:12 +0300

    Edit widget

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-import PopoverWidget from './widget';
+import PopoverWidget from '../widgets/popover/popover';
 
 const widget = new PopoverWidget(document.querySelector('#widget-container'),
     'Click to toggle popover', 'Popover title', "And here's some amazing content. It's very engaging. Right?");
